db9f1a2ffdda98f65f4650ed892f31b41f7648f6
rohverlive <rv484507@gmail.com>
2023-10-26 11:37:56 +0530

    i added a div

--- index.php
+++ index.php
@@ -32,6 +32,9 @@
     <p>This is a simple example of an interactive PHP page with Bootstrap.</p>
     <button class="btn btn-primary">Click Me</button>
 </div>
+<div>
+  hello kunal
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
